0ad390fcb825a653d285307f545cb2a022a78af1
MLReinforcify <160000823+MLReinforcify@users.noreply.github.com>
2024-03-11 16:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,42 @@
-
-from transformers import pipeline
-from transformers import AutoModelForSequenceClassification
-
-pipe = pipeline(model="facebook/bart-large-mnli")
-def tag(texts):
-    output = pipe(texts, candidate_labels=['Upgrades','Partnerships','Team updates','Events','Market insights','ICO announcements','White Paper release','ICO updates', 'Others',
-'NFT release','Partnership with NFT artists','Platform updates','Community discussion / AMAs','Airdrops','Contests / Give-aways','Regulatory updates','Security updates'
-],)
-    return output
+#necessary libraries
+import requests
+from bs4 import BeautifulSoup
+import csv
+import pandas as pd
 
 from flask import Flask, request, jsonify
 application = Flask(__name__)
 app=application
-@app.route('/tagging', methods=['POST'])
-def tags():
+@app.route('/categories', methods=['POST'])
+def scrape_data():
+    url = "https://coinmarketcap.com/cryptocurrency-category/"  
+
     try:
-        data = request.get_json()
-        texts = data['text']
-        result = tag(texts)
-        return jsonify(result)
-    except Exception as e:
-        return jsonify({'error': str(e)})
+        # request to the URL
+        response = requests.get(url)
+
+        # Check if the request is successful or not
+        if response.status_code == 200:
+            # Parse the HTML content
+            soup = BeautifulSoup(response.text, 'html.parser')
+            # Find the table in of the webpage
+            table = soup.find('table')
+            #table extraction
+            if table:
+                rows = table.find_all('tr')
+                data = []
+                for row in rows:
+                    cols = row.find_all(['td', 'th'])
+                    cols = [col.text.strip() for col in cols]
+                    data.append(cols)
+                result = [[row[1], row[4]] for row in data[1:13]]
+                return(jsonify(result))
+                #for row in result:
+                    #print(row)
+    
+    except requests.RequestException as e:
+        # Handle exceptions (connection errors or timeouts)
+        print(f"Error: {e}")
+
 if __name__ == '__main__':
     app.run(debug=True)
