77e0f64ae1c33e61bb75595767975a616b4351e4
MLReinforcify <160000823+MLReinforcify@users.noreply.github.com>
2024-03-12 14:41:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,28 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 import pandas as pd
+import re
 
+def money(value):
+    pattern = re.compile(r'\$([\d,]+)(\d+\.\d+)%')
+    match = pattern.match(value)
+    if match:
+        money_value = match.group(1)
+        percent_value = match.group(2)
+        return money_value, percent_value
+    else:
+        return 0
 from flask import Flask, request, jsonify
 application = Flask(__name__)
 app=application
-@app.route('/categories', methods=['POST'])
-def scrape_data():
-    url = "https://coinmarketcap.com/cryptocurrency-category/"  
-
+@app.route('/tokens', methods=['POST'])
+def scrape_data():   
     try:
-        # request to the URL
+        url = "https://coinmarketcap.com/cryptocurrency-category/"  
         response = requests.get(url)
-
-        # Check if the request is successful or not
         if response.status_code == 200:
-            # Parse the HTML content
             soup = BeautifulSoup(response.text, 'html.parser')
-            # Find the table in of the webpage
             table = soup.find('table')
-            #table extraction
             if table:
                 rows = table.find_all('tr')
                 data = []
@@ -30,12 +33,13 @@ def scrape_data():
                     cols = [col.text.strip() for col in cols]
                     data.append(cols)
                 result = [[row[1], row[4]] for row in data[1:13]]
-                return(jsonify(result))
-                #for row in result:
-                    #print(row)
-    
+                #print((result))
+        df = pd.DataFrame(result, columns=['Name', 'Value'])
+        df[['Money_Value', 'Percent_Value']] = df['Value'].apply(lambda x: pd.Series(money(x)))
+        #print(df['Name', 'Money_Value'])
+        return(df.iloc[:, [0, 2]].values.tolist())
+
     except requests.RequestException as e:
-        # Handle exceptions (connection errors or timeouts)
         print(f"Error: {e}")
 
 if __name__ == '__main__':
