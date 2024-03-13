fea7c28017430682b6f7e7c564a33d4dc8e9ce01
MLReinforcify <160000823+MLReinforcify@users.noreply.github.com>
2024-03-13 10:09:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+"""
 #necessary libraries
 import requests
 from bs4 import BeautifulSoup
@@ -44,3 +53,4 @@ def scrape_data():
 
 if __name__ == '__main__':
     app.run(debug=True)
+"""
